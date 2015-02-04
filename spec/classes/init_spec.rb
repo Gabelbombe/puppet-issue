0d7037b92a75816433565182034e5b0cce7fb5b4
ehime <dodomeki@gmail.com>
2015-02-04 13:47:04 -0800

    Adding invalid path

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,8 +9,31 @@ describe 'issue' do
     it { should contain_class('issue') }
 
     it {
-      should contain_file('issue').with({})
+      should contain_file('issue').with({
+        'ensure'  => 'file',
+        'path'    => '/etc/issue',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+        'content' => nil,
+      })
     }
+  end
 
+  describe 'with invalid path' do
+
+    ['issue'].each do |resource|  ## This allows us to be expandable later
+
+      context "specified for #{resource}_file" do
+
+        let (:params) {{ :"#{resource}_file" => 'invalid/path' }}
+
+        it 'should fail' do
+          expext {
+            should contain_class('issue')
+          }.to raise_error(Puppet::Error)
+        end
+      end
+    end
   end
 end
