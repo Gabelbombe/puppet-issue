987e3dc3c3c7ba9fe9dc3e6c7e87bbc7001d53dd
ehime <dodomeki@gmail.com>
2015-02-04 13:21:18 -0800

    Added file statement

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,4 +17,13 @@ class issue (
   validate_absolute_path($issue_file)
 
   validate_re($issue_mode, '^\d{4}$', 'vim::issue_mode does not match regexp. Must be four digit string.')
+
+  file {
+    ensure  => $issue_ensure,
+    path    => $issue_file,
+    owner   => $issue_owner,
+    group   => $issue_group,
+    mode    => $issue_mode,
+    content => $issue_content,
+  }
 }
